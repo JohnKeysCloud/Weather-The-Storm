82c08c0a79fdac0ed990c40f47b1f2fabc73dcf7
BL1ZZY <johnblairofficial@gmail.com>
2022-01-09 16:01:39 -0500

    CYCLONE

---
+++ CYCLONE.js
@@ -0,0 +1,48 @@
+//INTERACTIVE EXAMPLE:
+
+CYCLONE:
+// ndv78tscy_o aloOi_8u n78_slakOis ioo4Ps2tlO njlNalsC@_k 
+// mkldz9 @8Iix otLk_87jn o8C_5eCav mk78_akuzI xtO < 3SBB < 3x 
+// mkl8_DFoLe djkn @_xk89s eNk778_r3 nlO7_oBz mk98_ @oxkz 
+// xfRoM < 3JBB < 3x njK87_acl4 Rta_x34lQ jK_aS73xmO mkL8_sDdN 
+// ml9Z_olAbe yOw64pU_X2v 8_zkKal8iu xka09ism ap2 @_J82l mNz9a_iXc 
+// xAraLEx_43va u @eJkLoHs nA29_iPls2 lA_s3o2BTd op0_2kdlm LeC23 @o_m yT2_2aloMw
+
+CLOUDS:
+[m-z\d@_]{7}
+[a-p\d_@]{8}
+[j-s\d_@]{8}
+[p-x\d_@]{9}
+[e-t_@]{8}
+
+//Step 1.Head over to https://RegEx101.com
+
+//Step 2.In the bar titled 'REGULAR EXPRESSIONS' find the second forward slash towards the end and click to enable the 'i' insensitive flag and disable all others (for this example).
+
+//Step 2.Copy/paste the 'CYLCONE' into the bar titled "TEST STRING."
+
+//Step 3.Nowwww.... If ya will :), copy and paste the FIRST of the CLOUDs listed above ([m-z\d@_]{7}) into the "REGULAR EXPRESSION" bar on top of the page (beware of any whitespaces in the bar).
+//A fragment of the CYCLONE should now be... ILLUMINATED ( ◡‿◡ ).
+
+//Step 4.Write down in order from left to right, the characters included in the fragment that are NOT highlighted/targeted by the 'CLOUD'. This should leave you with your first ‘wonderBit’: "ilO"
+
+//Step 5.Move on to the next cloud. Copy/paste it into the “REGULAR EXPRESSION BAR” and follow “Step 4” again :) 
+
+//Step 6. Copy/paste the rest of the 'CLOUDs' into the "REGULAR EXPRESSION" bar ONE BY ONE (in order, without whitespaces) and follow "Step 4" again for each. You should end up with the following 'wonderBits':
+
+//........Cloud 1: "ilO"
+//........Cloud 2: "v"
+//........Cloud 3: "e"
+//........Cloud 4: "yO"
+//........Cloud 5: "u"
+
+//Step 7.Piece the 'wonderBits' together, writing them all out in order. This should leave you with our full ‘WONDER!:
+
+//“iloveyOu" 
+
+//(´♡‿♡`)
+
+//johnbla1r
+//theWonderKid
+//cyclonestudios
+//timecapsuleny
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-The-Storm
+# Cyclone 
+a string of various characters embedded with 'WONDER' (a hidden message). 
+To be solved for through the use of one or more 'CLOUD(S)' (regular expressions).
+
+Steps to solve:
+
+1. Find the fragment of the 'CYCLONE' targeted by the first 'CLOUD'.
+2. Write down in order from left to right the characters in the fragment that AREN'T targeted by the 'CLOUD'.
+3. Move on to the next 'CLOUD' and repeat until all are solved for and the 'WONDER' is able to be deciphered. 
+
+
+
+APPLICATIONS:
+Military and/or Nerdy Love Letters
+
+-tWK
